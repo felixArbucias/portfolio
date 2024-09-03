f8a78296d53addc930f4f033e25f8e0c8dd05b06
Felix Arbucias <felixmekkel@gmail.com>
2024-09-03 01:01:48 -0400

    finished hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import profilePic from '../assets/profilepicBW.jpg';
+
 const Hero = () => {
     return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -6,10 +8,16 @@ const Hero = () => {
                 <div className="flex flex-col items-center lg:items-start">
                     <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Felix Arbucias</h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer / Powerlifter</span>
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                    <p className="my-2 max-w-xl py-6 font-thick">
                     Building projects and exploring new programming challenges is my passion. Alongside my dedication to software development, I am a collegiate powerlifter with a current total of around 1400 lbs. I am particularly fascinated by AI development and eager to deepen my understanding of large language models.
                     </p>
                 </div>
+            </div>
+            <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex justify-center">
+                    <img src={profilePic} alt="Felix!"/>
+                </div>
+
             </div>
         </div>
     </div>
