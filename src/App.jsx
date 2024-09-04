868cb96e4d36508377a8b8d2f1665c188c0ee8cc
Felix Arbucias <felixmekkel@gmail.com>
2024-09-03 22:30:31 -0400

    finished experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
 const App = () => {
     return (
         <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -14,6 +15,7 @@ const App = () => {
                 <Hero/>
                 <About />
                 <Technologies />
+                <Experience />
             </div>
         </div>
     );
---
+++ src/assets/experiences.js
@@ -0,0 +1,24 @@
+export const EXPERIENCES = [
+    {
+        year: 'August 2024 - Present',
+        role: 'Full Stack Developer',
+        company: 'NDA',
+        description: 'Contributed to an privately sourced repository for a server. Uses Vue3 and integrated older Vue2 code to the new Vue3 platform. Uses different databases hosting mock player values to test frontend and backend technologies.',
+        technologies: ["Vue3", "Node.JS", "Python", "HTML / CSS"],
+    },
+    {
+        year:'June 2024 - August 2024',
+        role: 'SWE Intern',
+        company: 'Airia',
+        description: 'Worked with React JS for the front end and .NET framework for the backend (in addition to python and JSON) to create prompt engineering workbench.',
+        technologies: ["React.JS", "Tailwind CSS", ".NET", "Python"],
+    },
+    {
+        year:'January 2024 - June 2024',
+        role: 'Research and Development Intern',
+        company: 'BaseCamp305',
+        description: 'Created multiple websites on Wix for Miami Food and Wine Festival, securing them under a private domain with Scaleflex. ',
+        technologies: ["Javascript", "HTML", "CSS", "Wix", "Java"],
+    },
+]
+export default EXPERIENCES;
