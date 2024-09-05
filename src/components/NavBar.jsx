97f5662c9067e096bae5bf752846203286b019a2
Felix Arbucias <felixmekkel@gmail.com>
2024-09-05 01:19:51 -0400

    fixed links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,8 +7,12 @@ const NavBar = () => {
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className = "flex flex-shrink-0 items-center"></div>
             <div className = 'm-8 flex items-center justify-center gap-4 text-2xl'>
+                <a href='https://www.linkedin.com/in/felixarbucias/'>
                 <FaLinkedin />
+                </a>
+                <a href="https://github.com/felixArbucias">
                 <FaGithub />
+                </a>
             </div>
         </nav>
     );
